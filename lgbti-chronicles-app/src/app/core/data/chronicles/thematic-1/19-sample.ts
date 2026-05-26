ef8e6fd363bf1b106b20ad90de83d3c120a4f059
sergiopaz-bit <sergio.paz@udea.edu.co>
2026-05-26 12:18:26 -0500

    Update 19-sample.ts

--- lgbti-chronicles-app/src/app/core/data/chronicles/thematic-1/19-sample.ts
+++ lgbti-chronicles-app/src/app/core/data/chronicles/thematic-1/19-sample.ts
@@ -7,7 +7,7 @@ export const CHRONICLE_T1_19: LocalizedModalContent = {
     en: 'Ximena',
   },
   rawText: {
-    es: `<p>Cuánto dolor siento al saber<br>
+    es: `<p><i>Cuánto dolor siento al saber<br>
 de tantos seres desaparecidos,<br>  
 sin saber nada de ellos, sepultados en las escombreras.<br> 
 Cuánta tristeza siento,<br> 
@@ -16,29 +16,29 @@ Se les arrebató la felicidad y los condenaron al olvido.<br>
 ¿Dónde están? ¿Quiénes son esos hombres<br> 
 que se los llevaron?<br> 
 ¿O acaso ya olvidaron que también<br> 
-eran seres humanos?</p><p><i>Mujer, mestiza, urbana</i></p>
+eran seres humanos?</i></p><p><i>Mujer, mestiza, urbana</i></p>
 `,
-    fr: `<p>Quel deuil je ressens en sachant<br>   
+    fr: `<p><i>Quel deuil je ressens en sachant<br>   
 Que tous ces êtres ont disparus,<br>  
-Sans rien savoir d’eux, ensevelis sous les <b><u><i>{1}escombreras{/1}</i></u></b><br>
+Sans rien savoir d’eux, ensevelis sous les <b><u>{1}escombreras{/1}</u></b><br>
 Quelle tristesse je ressens,<br>   
 En me demandant s'ils ont faim ou froid.<br>  
 Leur bonheur leur a été arraché, et on les a condamnés à être oubliés.<br>   
 Où sont-ils ? Qui sont ces hommes<br>    
 qui les ont enlevés ?<br>  
 Ou ont-ils déjà oublié<br>   
-Qu'ils étaient, eux aussi, des êtres humains ?</p><p><i>Femme, métisse, citadine</i></p>
+Qu'ils étaient, eux aussi, des êtres humains ?</i></p><p><i>Femme, métisse, citadine</i></p>
 `,
-    en: `<p>How much pain I feel to know<br>
+    en: `<p><i>How much pain I feel to know<br>
 of so many disappeared ones<br> 
-knowing nothing from them, buried in the <b><u><i>{1}escombreras{/1}</i></u></b>.<br> 
+knowing nothing from them, buried in the <b><u>{1}escombreras{/1}</u></b>.<br> 
 How much sorrow I feel to think,<br> 
 if they are hungry or if they are cold.<br> 
 Their happiness was ripped out of them, and they were sentenced to be forgotten.<br> 
 Where are they? Who are those men who<br>  
 took them away?<br> 
 Or have they already forgotten that they<br> 
-were human beings too?</p><p><i>Woman, mestiza, urbanite</i></p>
+were human beings too?</i></p><p><i>Woman, mestiza, urbanite</i></p>
 `,
   },
   tooltips: {
