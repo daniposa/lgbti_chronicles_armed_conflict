8c7714ccbe21d8b6608a231cec7a2695fdd2aeee
sergiopaz-bit <sergio.paz@udea.edu.co>
2026-06-02 09:25:01 -0500

    Update 41-sample.ts

--- lgbti-chronicles-app/src/app/core/data/chronicles/thematic-3/41-sample.ts
+++ lgbti-chronicles-app/src/app/core/data/chronicles/thematic-3/41-sample.ts
@@ -8,18 +8,18 @@ export const CHRONICLE_T3_41: LocalizedModalContent = {
   },
   rawText: {
     es: `<p>Así lo expresó Carol, una mujer víctima de violencia sexual por parte del Frente 32 de las FARC-EP, en el Putumayo: «No sé... hubo un tiempo en que yo quería matarlos, matarlos con mis propias manos; yo deseaba que les hicieran todo lo que ellos me habían hecho, yo quería eso para ellos. Pero ahora solamente quiero que mi Dios los perdone, y que me dé la fuerza para yo poderlos perdonar también, no pido nada más».</p><p><i>Mujer, campesina, mestiza</i></p>`,
-    fr: `<p>Carol, femme victime de violence sexuelle par le <b><u>{1}32ème Front des FARC-EP{/1}</u></b> à Putumayo, a exprimé : « Bon… Il y a eu un temps où je voulais les tuer, les tuer de mes propres mains. Aussi, j’espérais que d’autres leur fassent subir les mêmes choses qu’ils m’avaient fait, je leur souhaitais du mal. Mais, maintenant, je veux que Dieu les pardonne et qu’il me donne la force de les pardonner aussi. C’est tout ce que je veux, je ne demande rien d’autre. »</p><p><i>Femme paysanne, métisse</i></p>`,
-    en: `<p>Carol, a victim of sexual violence in Putumayo, by the <b><u>{1}32nd Front of FARC-EP{/1}</u></b> told it like this: “Well… There was a time where I wanted to kill them, kill them with my own hands. I wanted people to do to them the same thing they did to me. Yes, I wanted that for them. But now I only want God to forgive them and to give me the strength to forgive them too. That’s all I want.”</p><p><i>Woman, farmer, </i>mestiza</p>`,
+    fr: `<p>Carol, femme victime de violence sexuelle par le <b>{1}32ème Front des FARC-EP{/1}</b> à Putumayo, a exprimé : « Bon… Il y a eu un temps où je voulais les tuer, les tuer de mes propres mains. Aussi, j’espérais que d’autres leur fassent subir les mêmes choses qu’ils m’avaient fait, je leur souhaitais du mal. Mais, maintenant, je veux que Dieu les pardonne et qu’il me donne la force de les pardonner aussi. C’est tout ce que je veux, je ne demande rien d’autre. »</p><p><i>Femme paysanne, métisse</i></p>`,
+    en: `<p>Carol, a victim of sexual violence in Putumayo, by the <b>{1}32nd Front of FARC-EP{/1}</b> told it like this: “Well… There was a time where I wanted to kill them, kill them with my own hands. I wanted people to do to them the same thing they did to me. Yes, I wanted that for them. But now I only want God to forgive them and to give me the strength to forgive them too. That’s all I want.”</p><p><i>Woman, farmer, </i>mestiza</p>`,
   },
   tooltips: {
     1: {
       es: `<p>Nota de ejemplo para la crónica T3-41.</p>`,
       fr: `<p><strong>Le 32ème Front des FARC-EP</strong></p>
       <p>Le 32ème Front des Forces armées révolutionnaires de Colombie (FARC) impose une série de restrictions aux habitants du sud du département de Putumayo afin de préserver ce qu'ils appellent «la coexistence pour le bon fonctionnement des communautés. »</p>
-      <p><a href="https://insightcrime.org/news/brief/document-exposes-iron-grip-of-farc-in-southern-colombia/" target="_blank" rel="noopener noreferrer">InSight Crime</a></p>`,
+      <p><a href="https://web.archive.org/web/20250226103023/https://insightcrime.org/news/brief/document-exposes-iron-grip-of-farc-in-southern-colombia/" target="_blank" rel="noopener noreferrer">InSight Crime</a></p>`,
       en: `<p><strong>32nd Front of FARC-EP</strong></p>
       <p>The 32nd Front of the Revolutionary Armed Forces of Colombia (FARC) enforces a set of restrictions on residents in southern Putumayo department to preserve what it calls “coexistence for well-functioning communities.”</p>
-      <p><a href="https://insightcrime.org/news/brief/document-exposes-iron-grip-of-farc-in-southern-colombia/" target="_blank" rel="noopener noreferrer">InSight Crime</a></p>`,
+      <p><a href="https://web.archive.org/web/20250226103023/https://insightcrime.org/news/brief/document-exposes-iron-grip-of-farc-in-southern-colombia/" target="_blank" rel="noopener noreferrer">InSight Crime</a></p>`,
     },
   },
   contextInfo: {
