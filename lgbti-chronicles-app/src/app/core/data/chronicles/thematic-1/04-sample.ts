07f2bbea1cc46b302388307c3ec17f8d11bc1e7c
sergiopaz-bit <sergio.paz@udea.edu.co>
2026-06-08 15:14:14 -0500

    Update 04-sample.ts

--- lgbti-chronicles-app/src/app/core/data/chronicles/thematic-1/04-sample.ts
+++ lgbti-chronicles-app/src/app/core/data/chronicles/thematic-1/04-sample.ts
@@ -8,22 +8,22 @@ export const CHRONICLE_T1_04: LocalizedModalContent = {
   },
   rawText: {
     es: '<p>Cerca a Chigorodó está la vereda Tascón, del municipio de Dabeiba. Allí, varios integrantes del Frente 5 de las FARC-EP, incluido un tío de la víctima, violentaron a Angie, mujer lesbiana que entre 1991 y 1994 fue sometida a violaciones, amenazas y tortura, cuando apenas tenía 12 años: «Esto me pasó en varias ocasiones en diferentes veredas. Quedé embarazada de uno de los miembros de las FARC, llamado Juan Carlos, que pertenecía al mismo frente de alias Karina. Por todo eso tuve que irme».</p><p><i>Mujer lesbiana</i></p>',
-    fr: '<p>Près de Chigorodó se trouve la <i><b>{1}vereda{/1}</b></i> de Tascón, dans la commune de Dabeiba. Là, plusieurs membres du 5e front des <b>{2}FARC-EP{/2}</b>, dont un oncle de la victime, ont agressé Angie, une femme lesbienne qui a été victime de viols, de menaces et de tortures entre 1991 et 1994, alors qu’elle n’avait que 12 ans : « Ça m’est arrivé à plusieurs reprises dans plein de veredas différentes. Je suis tombée enceinte d’un membre des FARC, un certain Juan Carlos, qui appartenait au même front dont faisait aussi partie celle connue sous le nom de Karina. C’est pour ça que j’ai dû partir. »</p><p><i>Femme lesbienne</i></p>',
-    en: '<p>Near the town of Chigorodó, one can find Tascón, a <i><b>{1}vereda{/1}</b></i> in the town of Dadeiba. There, several members of the 5th <b>{2}FARC-EP{/2}</b> Front, including the victim’s own uncle, violently assaulted Angie, a lesbian woman who, between 1991 and 1994, was subjected to rape, threats, and torture, when she was only 12 years old: “This happened to me on several occasions, in different veredas. I got pregnant by one of the FARC members, named Juan Carlos, who belonged to the same front as alias Karina. That is why I had to leave.”</p><p><i>Lesbian woman</i></p>',
+    fr: '<p>Près de Chigorodó se trouve la <i><b>{1}vereda{/1}</b></i> de Tascón, dans la commune de Dabeiba. Là, plusieurs membres du 5e front des <b>{2}FARC-EP{/2}</b>, dont un oncle de la victime, ont agressé Angie, une femme lesbienne qui a été victime de viols, de menaces et de tortures entre 1991 et 1994, alors qu’elle n’avait que 12 ans : « Ça m’est arrivé à plusieurs reprises dans plein de <i>veredas</i> différentes. Je suis tombée enceinte d’un membre des FARC, un certain Juan Carlos, qui appartenait au même front dont faisait aussi partie celle connue sous le nom de Karina. C’est pour ça que j’ai dû partir. »</p><p><i>Femme lesbienne</i></p>',
+    en: '<p>Near the town of Chigorodó, one can find Tascón, a <i><b>{1}vereda{/1}</b></i> in the town of Dadeiba. There, several members of the 5th <b>{2}FARC-EP{/2}</b> Front, including the victim’s own uncle, violently assaulted Angie, a lesbian woman who, between 1991 and 1994, was subjected to rape, threats, and torture, when she was only 12 years old: “This happened to me on several occasions, in different <i>veredas</i>. I got pregnant by one of the FARC members, named Juan Carlos, who belonged to the same front as alias Karina. That is why I had to leave.”</p><p><i>Lesbian woman</i></p>',
   },
   tooltips: {
     1: {
       es: '<p>Cerca a Chigorodó está la vereda Tascón, del municipio de Dabeiba. Allí, varios integrantes del Frente 5 de las FARC-EP, incluido un tío de la víctima, violentaron a Angie, mujer lesbiana que entre 1991 y 1994 fue sometida a violaciones, amenazas y tortura, cuando apenas tenía 12 años: «Esto me pasó en varias ocasiones en diferentes veredas. Quedé embarazada de uno de los miembros de las FARC, llamado Juan Carlos, que pertenecía al mismo frente de alias Karina. Por todo eso tuve que irme».</p><p><i>Mujer lesbiana</i></p>',
       fr: `<p><strong><i>Vereda</i></strong></p>
 <p><img src="images/flotantes/vereda.avif" alt="Une <i>finca</i> en Antioquia" width="220"></p>
-<p><small><i>Vereda</i> de Punta Larga à Popayán, dans le Cauca. On peut y accéder directement depuis l'autoroute panaméricaine</small></p>
+<p><small><i>Vereda</i> de Punta Larga à Popayán, dans le Cauca</small></p>
 <p>Selon le DANE (Département administratif national des statistiques), une <i>vereda</i> est une « division territoriale administrative dans les zones rurales des municipalités, établie par un accord municipal ».</p>
 <p>Au-delà de sa définition administrative, une <i>vereda</i> représente également un espace de vie communautaire rural très soudé. Comme les maisons et les <i>fincas</i> sont relativement proches les unes des autres, les habitants ont tendance à développer de bonnes relations avec le voisinage par le biais d'interactions quotidiennes.
 </p>
 <p><a href="https://web.archive.org/web/20260601215302/https://conceptos.dane.gov.co/conceptos/conceptos/6002/ficha/" target="_blank" rel="noopener noreferrer">DANE</a></p>`,
       en: `<p><strong><i>Vereda</i></strong></p>
 <p><img src="images/flotantes/vereda.avif" alt="Une <i>finca</i> en Antioquia" width="220"></p>
-<p><small><i>Vereda</i> of Punta Larga in Popayán, Cauca. It can be reached directly from Pan-American Highway</small></p>
+<p><small><i>Vereda</i> of Punta Larga in Popayán, Cauca</small></p>
 <p>According to the DANE (National Administrative Department of Statistics), a <i>vereda</i> is an “Administrative territorial division in rural areas of municipalities, established by municipal agreement.”</p>
 <p>Beyond its administrative definition, a <i>vereda</i> also represents a space of close-knit rural community life. Because homes and <i>fincas</i> are located in relative proximity, residents tend to develop strong neighborly relationships through daily interactions.
 </p>
@@ -45,6 +45,6 @@ target="_blank" rel="noopener noreferrer">Naciones Unidas</a>.</p>
   contextInfo: {
     es: '<p>Lugar: Vereda de Tascón, Dadeiba, Urabá antioqueño</p><p>Año: 1991-1994</p><p>Hecho(s) victimizante(s): violencia sexual, amenazas, tortura, desplazamiento, embarazo forzoso</p><p>Actor armado: Frente 5 de las FARC-EP</p>',
     fr: '<p>Lieu : <i>Vereda</i> de Tascón, Dadeiba, Urabá (Antioquia)</p><p>Date : 1991-1994</p><p>Fait(s) victimisant(s) : violence sexuelle, menaces, torture, déplacement, grossesse forcée</p><p>Acteur armé : Front 5 des FARC-EP</p>',
-    en: '<p>Place: Vereda of Tascón, Dadeiba, Urabá (Antioquia)</p><p>Date: 1991-1994</p><p>Victimizing act(s): sexual violence, threats, torture, displacement, forced pregnancy</p><p>Armed actor: FARC-EP 5th Front',
+    en: '<p>Place: <i>Vereda</i> of Tascón, Dadeiba, Urabá (Antioquia)</p><p>Date: 1991-1994</p><p>Victimizing act(s): sexual violence, threats, torture, displacement, forced pregnancy</p><p>Armed actor: FARC-EP 5th Front',
   },
 };
